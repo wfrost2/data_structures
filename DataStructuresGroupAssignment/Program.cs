2e15409f69b832f0a4cb985e602611cc45a8fd29
Wyatt Frost <wlf@byu.edu>
2017-10-03 11:37:29 -0600

    set up initial structure of program

--- DataStructuresGroupAssignment/Program.cs
+++ DataStructuresGroupAssignment/Program.cs
@@ -10,6 +10,39 @@ namespace DataStructuresGroupAssignment
     {
         static void Main(string[] args)
         {
+            //declare global variables. Don't forget to comment!!
+            int iDecision;
+            do
+            {//do while 
+                Console.WriteLine("1. Stack");
+                Console.WriteLine("2. Queue");
+                Console.WriteLine("3. Dictionary");
+                Console.WriteLine("4. Exit");
+
+                iDecision = Convert.ToInt32(Console.ReadLine());
+
+                switch(iDecision)
+                {
+                    case 1:
+                        //Logic for Stack
+
+
+                        break;
+                    case 2:
+                        //Logic for Queue
+
+
+                        break;
+                    case 3:
+                        //Logic for Dictionary
+
+
+
+                        break;
+                }
+
+            } while (iDecision != 4);
+
         }
     }
 }
